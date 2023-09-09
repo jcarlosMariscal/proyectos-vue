62160977c468dec52bfdd52746d1ed1f3eae92f7
JCarlosrm <juanc.ramirezm16@gmail.com>
2023-09-08 21:00:42 -0600

    Funcionalidad básica al primer proyecto

--- src/components/InputTag.vue
+++ src/components/InputTag.vue
@@ -1,10 +1,40 @@
 <template>
   <div class="inputTag">
-    <div class="tags"></div>
-    <div class="text"></div>
+    <div class="tags">
+      <div class="tag" v-for="(tag, index) in tags" :key="index">
+        {{ tag }} <button @click="deleteTag(tag)">X</button>
+      </div>
+    </div>
+    <form @submit.prevent="handleSubmit">
+      <input class="text" v-model="currentValue" />
+    </form>
   </div>
 </template>
 
-<script setup></script>
+<script>
+export default {
+  name: "input-tag",
+  data() {
+    return {
+      currentValue: "",
+      tags: [],
+    };
+  },
+  methods: {
+    handleSubmit() {
+      if (this.currentValue !== "") {
+        const exits = this.tags.some((item) => item === this.currentValue);
+        if (!exits) {
+          this.tags.push(this.currentValue);
+          this.currentValue = "";
+        }
+      }
+    },
+    deleteTag(tag) {
+      this.tags = this.tags.filter((item) => item !== tag);
+    },
+  },
+};
+</script>
 
 <style scoped></style>
